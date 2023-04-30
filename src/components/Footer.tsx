882de69698966588af15fd7100a611a7a3384d2b
Mir Sahib <mirsahib24@gmail.com>
2023-04-30 11:31:20 +0600

    :hammer: Add footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='w-full text-center p-5 text-primary'>
+      <p>All Rights Reserved. Copyright © {new Date().getFullYear()}</p>
+    </footer>
   )
 }
