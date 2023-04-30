65d9e4ba5d1145b20eb961c8e686a2e4e2294542
Mir Sahib <mirsahib24@gmail.com>
2023-04-30 19:31:39 +0600

    :hammer: Add static read function

---
+++ src/pages/lib/readById.ts
@@ -0,0 +1,55 @@
+import fsPromises from 'fs/promises';
+import { GetStaticProps,GetStaticPaths, GetStaticPropsContext } from 'next';
+import path from 'path'
+import { ParsedUrlQuery } from 'querystring';
+
+interface IParams extends ParsedUrlQuery{
+    plantslug:string
+}
+
+interface IPlant{
+    id:string,
+    name:string,
+    price:number,
+    description:string,
+    img:string
+}
+
+const getStaticPaths:GetStaticPaths = async () => {
+    const paths:any[] = []
+
+    for (let index = 1; index <=10; index++) {
+        paths.push('/plants/'+index)
+    }
+
+    return{
+        paths,fallback:true
+    }
+}
+
+const getStaticProps:GetStaticProps =  async (context)=> {
+    const filePath = path.join(process.cwd(), './src/mock/plant.json');
+    const jsonData = await fsPromises.readFile(filePath,'utf-8');
+    const plants:IPlant[] = JSON.parse(jsonData);
+    const {plantslug} = context.params as IParams
+    const plant = plants.find(item=>{
+        return item.id === plantslug
+    })
+    if(!plant){
+        return {
+            notFound:true
+        }
+    }
+    return {
+      props:{
+        plant
+      }
+    }
+  
+}
+
+
+export {
+    getStaticProps,
+    getStaticPaths
+}
