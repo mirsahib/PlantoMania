516d04828afcab01c755071cde25a6acca55f834
Mir Sahib <mirsahib24@gmail.com>
2023-05-01 20:02:45 +0600

    :hammer: Add redux provide

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
-import "@fortawesome/fontawesome-svg-core/styles.css"; 
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import Script from "next/script";
-config.autoAddCss = false; 
+import { Provider } from "react-redux";
+import store from "@/store";
 
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-			<Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js" strategy="beforeInteractive"/>
+			<Provider store={store}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</Provider>
+			<Script
+				src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"
+				strategy="beforeInteractive"
+			/>
 		</>
 	);
 }
