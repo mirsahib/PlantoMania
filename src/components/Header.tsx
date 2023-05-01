9997f3be7589d19e361228cd4d12bf29e0a0a9d1
Mir Sahib <mirsahib24@gmail.com>
2023-05-01 19:39:38 +0600

    :recycle: Add cart module component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faCartShopping} from '@fortawesome/free-solid-svg-icons'
-
+import {
+	faCartShopping,
+} from "@fortawesome/free-solid-svg-icons";
+import Cart from "../modules/cart/Cart";
 
 function Header() {
-  return (
-    <div className='py-6 mb-6 bg-primary'>
-      			<nav className="flex justify-between items-center w-[90%] max-w-screen-lg  mx-auto">
+	return (
+		<div className="py-6 mb-6 bg-primary">
+			<nav className="flex justify-between items-center w-[90%] max-w-screen-lg  mx-auto">
 				<div className="flex flex-row gap-8 items-center text-white ">
 					<Link href="/">
 						<h1 className="text-xl font-semibold">Plantomania</h1>
@@ -16,19 +18,34 @@ function Header() {
 				<ul className="flex gap-8 text-white">
 					<li className="font-semibold">
 						<Link href={"/"}>
-							<i className="fa fa-user mr-1" aria-hidden="true"></i>
+							<i
+								className="fa fa-user mr-1"
+								aria-hidden="true"
+							></i>
 						</Link>
 					</li>
-					<li className="font-semibold">
-						<Link href="/publish">
-							<FontAwesomeIcon icon={faCartShopping} className='pr-1'/>
-              Cart
-						</Link>
+					<li>
+						<button
+							// onClick={()=>{console.log("click")}}
+							className="font-semibold"
+							type="button"
+							data-drawer-target="drawer-right-example"
+							data-drawer-show="drawer-right-example"
+							data-drawer-placement="right"
+							aria-controls="drawer-right-example"
+						>
+							<FontAwesomeIcon
+								icon={faCartShopping}
+								className="pr-1"
+							/>
+							Cart
+						</button>
+						<Cart/>
 					</li>
 				</ul>
 			</nav>
-    </div>
-  )
+		</div>
+	);
 }
 
-export default Header
+export default Header;
