305853d909a633275d825575396e095ff299e332
Mir Sahib <mirsahib24@gmail.com>
2023-05-02 10:51:03 +0600

    :rotating_light: fix nextjs Props 'className' warning

--- src/modules/cart/Cart.tsx
+++ src/modules/cart/Cart.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import CartItem from "./components/CartItem";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
 export default function Cart() {
     
 
 	return (
 		<div
 			id="drawer-right-example"
-			className="fixed top-0 right-0 z-40 h-screen  overflow-y-auto transition-transform duration-300 translate-x-full bg-white lg:w-1/3 sm:w-1/2 dark:bg-gray-800"
+			className="fixed top-0 right-0 z-40 h-screen overflow-y-auto transition-transform duration-300 translate-x-full bg-white lg:w-1/3 sm:w-1/2 dark:bg-gray-800"
 			tabIndex={-1}
 			aria-labelledby="drawer-right-label"
 		>
@@ -24,19 +25,7 @@ export default function Cart() {
 					aria-controls="drawer-right-example"
 					className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
 				>
-					<svg
-						aria-hidden="true"
-						className="w-5 h-5"
-						fill="currentColor"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							fillRule="evenodd"
-							d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-							clipRule="evenodd"
-						></path>
-					</svg>
+					<FontAwesomeIcon   icon={faClose}/>
 				</button>
 			</div>
 			{/* cart header end */}
