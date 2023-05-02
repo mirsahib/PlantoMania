6a332abcd84366709f1594f9552bb886ce15c99d
Mir Sahib <mirsahib24@gmail.com>
2023-05-02 19:42:11 +0600

    :hammer: Add redux store logic

--- src/modules/cart/Cart.tsx
+++ src/modules/cart/Cart.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import CartItem from "./components/CartItem";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
-export default function Cart() {
-    
+import { useAppSelector } from "@/store";
 
+export default function Cart() {
+	const cartItem = useAppSelector((state) => state.cartStore);
 	return (
 		<div
 			id="drawer-right-example"
@@ -25,27 +26,47 @@ export default function Cart() {
 					aria-controls="drawer-right-example"
 					className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
 				>
-					<FontAwesomeIcon   icon={faClose}/>
+					<FontAwesomeIcon icon={faClose} />
 				</button>
 			</div>
 			{/* cart header end */}
-			<ul className="overflow-y-auto h-96">
-				<CartItem />
-				<CartItem />
-			</ul>
-			{/* item container end*/}
-			<div className="p-4">
-				<h4 className="text-gray-600 text-sm mb-1">
-					Shipping and taxes will be calculated at checkout.
-				</h4>
-				<div className="flex justify-between mr-3 mb-2 text-gray-900 text-lg">
-					<h3 className="">Total</h3>
-					<h3 className="">$39.00</h3>
+			{cartItem.totalItem == 0 ? (
+				<div className="flex flex-col h-[90%] items-center justify-center">
+					<h3 className="text-gray-500 text-xl mb-4">
+						Your cart is empty
+					</h3>
+					<button
+						type="button"
+						data-drawer-hide="drawer-right-example"
+						aria-controls="drawer-right-example"
+						className="text-blue-600 p-3 bg-sky-100 rounded"
+					>
+						Back to store
+					</button>
 				</div>
-				<button className=" bg-blue-600 hover:bg-blue-500 py-4 px-4 rounded w-full">
-					Checkout
-				</button>
-			</div>
+			) : (
+				<div>
+					<ul className="overflow-y-auto h-96">
+						{cartItem.itemList.map((item, index) => {
+							return <CartItem {...item} key={index} />;
+						})}
+					</ul>
+					{/* item container end*/}
+					<div className="p-4">
+						<h4 className="text-gray-600 text-sm mb-1">
+							Shipping and taxes will be calculated at checkout.
+						</h4>
+						<div className="flex justify-between mr-3 mb-2 text-gray-900 text-lg">
+							<h3 className="">Total</h3>
+							<h3 className="">${cartItem.totalPrice}</h3>
+						</div>
+						<button className=" bg-blue-600 hover:bg-blue-500 py-4 px-4 rounded w-full">
+							Checkout
+						</button>
+					</div>
+				</div>
+			)}
+
 			{/* checkout container */}
 		</div>
 	);
